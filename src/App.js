6005d9238d9c42dede12a5a2a5036c50db643aae
williamcd <wcdetrick@me.com>
2020-02-12 09:14:28 -0500

    added basic components and react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Login from './components/Login';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div>
-      test
+      <Router>
+        <Nav />
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/home" component={Home} />
+            {/* <Route exact path="/education" component={Education} /> */}
+            {/* <Route exact path="/affiliates" component={Affiliates} /> */}
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
